3ceb6e59b9fb671d5de010fd1ef13b98cd14e87c
Mariia Pidlisna <mariia.pidlisna@gmail.com>
2026-03-05 22:35:03 +0200

    Update developer name in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: Anastasiia Oliinyk</p>
+          <p>Developer: Pidlisna Mariia</p>
           <p>
             Contact us:
             <a href="mailto:student@notehub.app">student@notehub.app</a>
